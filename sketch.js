37b988cda200be7af1e2fe3e20fdf77d53ad9426
Luiz Pansarini <luiz.pansarini@uaubox.com.br>
2020-06-29 15:41:49 -0300

    Aula 1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let imagemCenario;
+let imagemPersonagem;
+let cenario;
+let somDoJogo;
+let personagem;
+
+function preload(){
+  imagemCenario = loadImage('assets/imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('assets/imagens/personagem/correndo.png');
+  somDoJogo = loadSound('assets/sons/trilha_jogo.mp3');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
+  personagem = new Personagem(imagemPersonagem);
+  frameRate(40);
+  //somDoJogo.loop();
 }
 
 function draw() {
-  background(220);
+  cenario.exibe();
+  cenario.move();
+  personagem.exibe();
 }
